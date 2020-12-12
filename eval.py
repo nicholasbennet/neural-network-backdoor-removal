d0b6f2679cc3b75c816f7aced62bd5c12cb54fda
nicholasbennet <bennet08121996nicholas@gmail.com>
2020-12-12 03:51:19 -0500

    Edited eval.py

---
+++ eval.py
@@ -0,0 +1,36 @@
+from tensorflow import keras
+import sys
+import h5py
+import numpy as np
+import tensorflow as tf
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.InteractiveSession(config=config)
+
+clean_data_filename = str(sys.argv[1])
+model_filename = str(sys.argv[2])
+
+def data_loader(filepath):
+    data = h5py.File(filepath, 'r')
+    x_data = np.array(data['data'])
+    y_data = np.array(data['label'])
+    x_data = x_data.transpose((0,2,3,1))
+
+    return x_data, y_data
+
+def data_preprocess(x_data):
+    return x_data/255
+
+def main():
+    x_test, y_test = data_loader(clean_data_filename)
+    x_test = data_preprocess(x_test)
+
+    bd_model = keras.models.load_model(model_filename)
+
+    clean_label_p = np.argmax(bd_model.predict(x_test), axis=1)
+    class_accu = np.mean(np.equal(clean_label_p, y_test))*100
+    print('Classification accuracy:', class_accu)
+
+if __name__ == '__main__':
+    main()
