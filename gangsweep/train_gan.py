a7ed1dee023a567b6c5d88f4d9b132c256d1bbac
Rakshitha Kori Raj <grakshu@gmail.com>
2020-12-23 03:20:12 -0500

    working gangsweep end to end

--- gangsweep/train_gan.py
+++ gangsweep/train_gan.py
@@ -18,6 +18,18 @@ TF_CONFIG_ = tf.compat.v1.ConfigProto()
 TF_CONFIG_.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config = TF_CONFIG_)
 
+clean_data_filename='data/clean_test_data.h5'
+validation_data_filename = 'data/clean_validation_data.h5'
+model_filename = 'models/anonymous_bd_net.h5'
+
+epochs = 4
+target_begin = 0
+
+target_end = 10
+
+# Prepare the training dataset.
+batch_size = 64
+
 def res_block_gen(model, kernal_size, filters, strides):
     
     gen = model
@@ -54,8 +66,7 @@ res = res_block_gen(prelu_1, 3, 32, 1)
 res = res_block_gen(res, 3, 32, 1)
 res = res_block_gen(res, 3, 32, 1)
 res = res_block_gen(res, 3, 32, 1)
-#res_3 = res_block_gen(res_2, 3, 64, 1)
-#res_4 = res_block_gen(res_3, 3, 64, 1)
+
 
 
 conv_2 = keras.layers.Conv2D(filters = 32, kernel_size = 3, strides = 1, padding = "same")(res)
@@ -69,26 +80,28 @@ model_gan = keras.models.Model(inputs = gen_x, outputs = act_1)
 model_gan.save("gan_orig.h5")
 #model_gan.summary()
 
-clean_data_filename='data/clean_test_data.h5'
-validation_data_filename = 'data/clean_validation_data.h5'
-model_filename = 'models/anonymous_bd_net.h5'
+
 x_train, y_train = data_loader(clean_data_filename)
 x_train = data_preprocess(x_train)
 
 #optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002)
 
-# Prepare the training dataset.
-batch_size = 64
+
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 train_dataset = train_dataset.shuffle(buffer_size=x_train.shape[0],seed=90).batch(batch_size)
 bd_model_original = keras.models.load_model(model_filename)
 bd_model_original.Training = False
-epochs = 4
-target_begin = 1
 
-target_end = 10
+bd_model = keras.models.load_model(model_filename)
+new_output = keras.layers.Dense(1284, activation='softmax',name='output')(bd_model.layers[-2].output)
+bd_model = keras.Model(inputs=bd_model.inputs, outputs=new_output)
+bd_model.save("models/anonymous_bd_net_latest_repaired.h5")
+x_valid, y_valid = data_loader(validation_data_filename)
+    
+x_valid = data_preprocess(x_valid)
 
 for target in range(target_begin,target_end):
+    
     K.clear_session()
     sess.close()
     sess = tf.compat.v1.Session(config = TF_CONFIG_)
@@ -149,13 +162,69 @@ for target in range(target_begin,target_end):
                 
             grads = tape.gradient(loss_value, model_gan.trainable_weights)
             optimizer.apply_gradients(zip(grads, model_gan.trainable_weights))
-            if step % 10 == 0:
+            if step % 50 == 0:
                 print(
                     "Training loss (for one batch) at step {}: target:{} l_adv:{} l_pert:{} loss:{} ".format(step,target,l_adv,l_pert,loss_value)
                 )
                 print("Seen so far: %s samples" % ((step + 1) * 64))
-
+    
+    
+    
+    
+    
+    
     model_gan.save("models/trigger_gen/trigger_gen_{}.h5".format(target))
+    
+    # Calcualte success rate
+    
+    model_gan = keras.models.load_model("models/trigen_tues_01/trigger_gen_{}.h5".format(target))
+    
+    #x_valid, y_valid = data_loader(validation_data_filename)
+    
+    #x_valid = data_preprocess(x_valid)
+    y_valid_10_arg = np.random.choice(tf.where(y_valid!=target).numpy().flatten(),int(y_valid.shape[0]*0.1))
+    x_valid_10 =  x_valid.numpy()[y_valid_10_arg,:]
+    y_valid_10 =  y_valid[y_valid_10_arg]
+    logits = model_gan(
+        x_valid_10,
+        training=False
+    )
+    #Scale between 0 and 1
+    epsilon = 1e-12 
+    logits = tf.div(
+        tf.subtract(logits, tf.reduce_min(logits)),
+        tf.math.maximum( 
+            tf.subtract(tf.reduce_max(logits), tf.reduce_min(logits)),epsilon)
+        )
+    y_valid_pred = tf.argmax(bd_model_original(
+                    tf.clip_by_value(x_valid_10+logits,0,1), 
+                    training=False),axis = 1)
+    success_rate = int(np.argwhere(y_valid_pred.numpy()==target).shape[0])/ int(y_valid_pred.shape[0]) 
+    print("target {} ;success_rate {}".format(target,success_rate))
+    if(success_rate>0.79):
+        def refine(x_valid_10,y_valid_10,logits,y_valid_pred,target):
+            x_test_poisoned = x_valid_10 + logits
+            x_test_poisoned =  x_test_poisoned.numpy()[np.argwhere(y_valid_pred.numpy()==target).flatten(),:]
+            x_test = np.vstack((x_train, x_test_poisoned))
+            y_test = np.hstack((y_train, np.zeros(x_test_poisoned.shape[0])+1283))
+            '''
+            train_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+            
+            # Shuffle and slice the dataset.
+            train_dataset = train_dataset.shuffle(buffer_size=y_test.shape[0]).batch(batch_size)
+            '''
+            bd_model = keras.models.load_model("models/anonymous_bd_net_latest_repaired.h5")
+            bd_model.compile(
+                optimizer=bd_model_original.optimizer,
+                loss=bd_model_original.loss,
+                metrics=['accuracy']
+                )
+            bd_model.fit(x_test,y_test,shuffle=True, batch_size=64,epochs=5)
+            bd_model.save("models/anonymous_bd_net_latest_repaired.h5")
+            bd_model.save("models/anonymous_bd_net_targ_{}_repaired.h5".format(target))
+            del bd_model
+        refine(x_valid_10,y_valid_10,logits,y_valid_pred,target)
     del model_gan
-    del optimizer
+    #del optimizer
+    #del bd_model
      
